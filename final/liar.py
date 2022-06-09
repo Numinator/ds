d943ebdb832e348105aba0a28f1ac83d615bbda3
Frederik Kallestrup Mastratisi <frederik.mastratisi@hotmail.com>
2022-06-09 21:03:24 +0200

    liar

--- final/liar.py
+++ final/liar.py
@@ -18,9 +18,8 @@ from sklearn import linear_model, metrics, naive_bayes, svm
 # from keras.models import Sequential
 
 import psycopg2
-liar_tests = pd.read_csv("./test.tsv", sep='\t', header=None)
-liar_trains = pd.read_csv("./train.tsv", sep='\t', header=None)
-liar_valid = pd.read_csv("./valid.tsv", sep='\t', header=None)
+
+
 
 def truth_mapper(value):
     true_set = {'true', 'half-true', 'barely-true', 'mostly-true'}
@@ -32,10 +31,16 @@ def truth_mapper(value):
     else:
         raise ValueError
 
-def map_truth(liar_df):
+def map_project_truth(liar_df):
     truths = liar_df[1]
-    liar_df[1] = truths.apply(truth_mapper)
-    return liar_df
+    return truths.apply(truth_mapper)
 
 
-    
+
+def get_liar():
+    liar_tests = pd.read_csv("./test.tsv", sep='\t', header=None)
+    liar_trains = pd.read_csv("./train.tsv", sep='\t', header=None)
+    liar_valid = pd.read_csv("./valid.tsv", sep='\t', header=None)
+    liar = pd.concat([liar_tests, liar_trains, liar_valid], ignore_index = True)
+    return liar[2], map_project_truth(liar)
+
